cc6d6099c97191da18dde3ff6cb526547fc07b47
Natik Alam Tahsin <natikalamtahsin@gmail.com>
2023-06-11 12:57:53 +0600

    Auth Provider

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,16 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthProvider from "./provider/AuthProvider";
+
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <QueryClientProvider client={queryClient}>
-    <React.StrictMode>
-      <RouterProvider router={router}></RouterProvider>
-    </React.StrictMode>
-  </QueryClientProvider>
+  <React.StrictMode>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router}></RouterProvider>
+      </QueryClientProvider>
+    </AuthProvider>
+  </React.StrictMode>
 );
---
+++ src/provider/AuthProvider.jsx
@@ -0,0 +1,92 @@
+import axios from "axios";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  updateProfile,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { app } from "../firebase/firebase.config";
+
+const AuthContext = createContext();
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  /* Google SignIn */
+  const GoggleProvider = new GoogleAuthProvider();
+  const googleSignIn = () => {
+    setLoading(true);
+    return signInWithPopup(auth, GoggleProvider);
+  };
+  /**/
+
+  /* SignIn */
+  const SignIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  /**/
+
+  /* signUP */
+  const signUp = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  /**/
+
+  /* Update Users */
+  const updateUser = (name, photoUrl) => {
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photoUrl,
+    });
+  };
+  /**/
+
+  /* logOut */
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+  /**/
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      if (currentUser) {
+        axios
+          .post("http://localhost:5000/jwt", { email: currentUser.email })
+          .then((res) => {
+            console.log(res.data.token);
+            localStorage.setItem("access-token", res.data.token);
+            setLoading(false);
+          });
+      } else {
+        localStorage.removeItem("access-token");
+      }
+    });
+    return () => {
+      return unsubscribe();
+    };
+  }, []);
+
+  const authInfo = {
+    user,
+    loading,
+    googleSignIn,
+    SignIn,
+    signUp,
+    updateUser,
+    logOut,
+  };
+  return <AuthContext.Provider value={authInfo}></AuthContext.Provider>;
+};
+
+export default AuthProvider;
